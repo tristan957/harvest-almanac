baaf8d7a3347d1f8a359eaac09a2bd4ad727b2df
Tristan Partin <tristan.partin@gmail.com>
2019-07-07 18:43:39 -0500

    libharvest-glib: Use GDateTime over char *

--- lib/harvest-client.c
+++ lib/harvest-client.c
@@ -14,8 +14,8 @@ struct _HarvestClient
 	gboolean is_active;
 	char *address;
 	char *currency;
-	char *created_at;
-	char *updated_at;
+	GDateTime *created_at;
+	GDateTime *updated_at;
 };
 
 G_DEFINE_TYPE(HarvestClient, harvest_client, G_TYPE_OBJECT)
@@ -43,8 +43,10 @@ harvest_client_finalize(GObject *obj)
 	g_free(self->name);
 	g_free(self->address);
 	g_free(self->currency);
-	g_free(self->created_at);
-	g_free(self->updated_at);
+	if (self->created_at != NULL)
+		g_date_time_unref(self->created_at);
+	if (self->updated_at != NULL)
+		g_date_time_unref(self->updated_at);
 
 	G_OBJECT_CLASS(harvest_client_parent_class)->finalize(obj);
 }
@@ -71,10 +73,10 @@ harvest_client_get_property(GObject *obj, guint prop_id, GValue *val, GParamSpec
 		g_value_set_string(val, self->currency);
 		break;
 	case PROP_CREATED_AT:
-		g_value_set_string(val, self->created_at);
+		g_value_set_boxed(val, self->created_at);
 		break;
 	case PROP_UPDATED_AT:
-		g_value_set_string(val, self->updated_at);
+		g_value_set_boxed(val, self->updated_at);
 		break;
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID(obj, prop_id, pspec);
@@ -107,12 +109,15 @@ harvest_client_set_property(GObject *obj, guint prop_id, const GValue *val, GPar
 		self->currency = g_value_dup_string(val);
 		break;
 	case PROP_CREATED_AT:
-		g_free(self->created_at);
-		self->created_at = g_value_dup_string(val);
+		if (self->created_at != NULL)
+			g_date_time_unref(self->created_at);
+		self->created_at = g_value_dup_boxed(val);
 		break;
 	case PROP_UPDATED_AT:
-		g_free(self->updated_at);
-		self->updated_at = g_value_dup_string(val);
+
+		if (self->updated_at != NULL)
+			g_date_time_unref(self->updated_at);
+		self->updated_at = g_value_dup_boxed(val);
 		break;
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID(obj, prop_id, pspec);
@@ -141,10 +146,11 @@ harvest_client_class_init(HarvestClientClass *klass)
 	obj_properties[PROP_CURRENCY]   = g_param_spec_string("currency", _("Currency"),
 		  _("The currency code associated with this client."), NULL,
 		  G_PARAM_READWRITE | G_PARAM_CONSTRUCT);
-	obj_properties[PROP_CREATED_AT] = g_param_spec_string("created_at", _("Created At"),
-		_("Date and time the client was created."), NULL, G_PARAM_READWRITE | G_PARAM_CONSTRUCT);
-	obj_properties[PROP_UPDATED_AT] = g_param_spec_string("updated_at", _("Updated At"),
-		_("Date and time the client was last updated."), NULL,
+	obj_properties[PROP_CREATED_AT] = g_param_spec_boxed("created_at", _("Created At"),
+		_("Date and time the client was created."), G_TYPE_DATE_TIME,
+		G_PARAM_READWRITE | G_PARAM_CONSTRUCT);
+	obj_properties[PROP_UPDATED_AT] = g_param_spec_boxed("updated_at", _("Updated At"),
+		_("Date and time the client was last updated."), G_TYPE_DATE_TIME,
 		G_PARAM_READWRITE | G_PARAM_CONSTRUCT);
 
 	g_object_class_install_properties(obj_class, N_PROPS, obj_properties);
